fd29d61d8f931ccdfbeaf940e41ba45d79c3a7ec
LuK1337 <priv.luk@gmail.com>
2023-07-09 13:51:01 +0200

    extra: Build ih8sn inline
    Change-Id: I46c47da685bfe837aa6889d3cc110e66c41e5645
    Signed-off-by: x0x00044 <zlewchan@outlook.com>

--- product.mk
+++ product.mk
@@ -4,7 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+EXTRA_PATH := vendor/extra
+
 # Google Apps
 ifeq ($(WITH_GMS), true)
 $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+
+# ih8sn
+ifneq (,$(wildcard vendor/ih8sn))
+$(call inherit-product-if-exists, $(EXTRA_PATH)/ih8sn/ih8sn.mk)
+endif
 endif
