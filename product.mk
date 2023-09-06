5f7fb9a4467a5820a44e49b7577876bd30b20e0c
x0x00044 <zlewchan@outlook.com>
2023-09-06 16:18:07 +0200

    extra: Guard ih8sn

--- product.mk
+++ product.mk
@@ -11,7 +11,7 @@ ifeq ($(WITH_GMS), true)
 $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
 
 # ih8sn
-ifneq (,$(wildcard vendor/ih8sn))
+ifeq ($(WITH_IH8SN), true)
 $(call inherit-product-if-exists, $(EXTRA_PATH)/ih8sn/ih8sn.mk)
 endif
 endif
