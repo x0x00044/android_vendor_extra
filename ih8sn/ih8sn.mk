982b03ef40b2f3d56bf1521aed8eefbaa8b2bdc1
x0x00044 <zlewchan@outlook.com>
2023-12-07 23:45:11 +0100

    extra: Build ih8sn no matter the config situation
    Private config momento

--- ih8sn/ih8sn.mk
+++ ih8sn/ih8sn.mk
@@ -1,6 +1,4 @@
 IH8SN_PATH := $(EXTRA_PATH)/ih8sn
 
-ifneq (,$(wildcard $(IH8SN_PATH)/ih8sn.conf))
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += system/bin/ih8sn system/etc/ih8sn.conf system/etc/init/ih8sn.rc
 PRODUCT_PACKAGES += ih8sn
-endif
