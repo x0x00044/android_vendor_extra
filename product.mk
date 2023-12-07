dff24e3d06778354a2245d858cee16d7638c4a8b
x0x00044 <zlewchan@outlook.com>
2023-12-07 20:45:03 +0100

    extra: Refactor ih8sn impl

--- product.mk
+++ product.mk
@@ -10,12 +10,15 @@ EXTRA_PATH := vendor/extra
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(EXTRA_PATH)/overlay
 PRODUCT_PACKAGE_OVERLAYS += $(EXTRA_PATH)/overlay
 
+# Private vendor
+$(call inherit-product-if-exists, vendor/x0x00044/product.mk)
+
 # Google Apps
 ifeq ($(WITH_GMS), true)
 $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
 
 # ih8sn
-ifeq ($(WITH_IH8SN), true)
+ifneq (,$(wildcard vendor/ih8sn))
 $(call inherit-product-if-exists, $(EXTRA_PATH)/ih8sn/ih8sn.mk)
 endif
 endif
